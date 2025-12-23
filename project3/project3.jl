3d600fdf592fb6193e2c057a9f47fa45601be8f8
Romeo Valentin <romeov@stanford.edu>
2025-12-22 16:50:14 -0800

    fixup! Add custom diff for encrypted p0 file

--- project3/project3.jl
+++ project3/project3.jl
@@ -727,7 +727,7 @@ end
 # ╔═╡ 307afd9c-6dac-4a6d-89d7-4d8cabfe3fe5
 Markdown.MD(
 	md"""
-  $(@bind rerun_small LargeCheckBox(text="⟵ Click to re-run the <code>SmallSystem</code> evaluation.", default=parse(Bool, get(ENV, "AA228V_RUN_EVALS", "false"))))""",
+  $(@bind rerun_small LargeCheckBox(text="⟵ Click to re-run the <code>SmallSystem</code> evaluation.", default=parse(Bool, get(ENV, "AA228V_CI_RUN_EVALS", "false"))))""",
 	Markdown.parse("""
 	↑ This will re-run **`estimate_reachable_sets(::SmallSystem, ψ)`** and re-save **`$(get_filename(sys_small, ThisProject))`**
 
@@ -738,7 +738,7 @@ Markdown.MD(
 # ╔═╡ 38f26afd-ffa5-48d6-90cc-e3ec189c2bf1
 Markdown.MD(
 	md"""
-  $(@bind rerun_medium LargeCheckBox(text="⟵ Click to re-run the <code>MediumSystem</code> evaluation.", default=parse(Bool, get(ENV, "AA228V_RUN_EVALS", "false"))))""",
+  $(@bind rerun_medium LargeCheckBox(text="⟵ Click to re-run the <code>MediumSystem</code> evaluation.", default=parse(Bool, get(ENV, "AA228V_CI_RUN_EVALS", "false"))))""",
 	Markdown.parse("""
 	↑ This will run **`estimate_reachable_sets(::MediumSystem, ψ)`** and save **`$(get_filename(sys_medium, ThisProject))`**
 
@@ -1012,7 +1012,7 @@ We'll automatically test your `estimate_reachable_sets(::LargeSystem, ψ)` funct
 # ╔═╡ 7fe1c3d7-469c-47d9-9d46-e5b8b263edb9
 Markdown.MD(
 	md"""
-  $(@bind rerun_large LargeCheckBox(text="⟵ Click to re-run the <code>LargeSystem</code> evaluation."), default=parse(Bool, get(ENV, "AA228V_RUN_EVALS", "false"))""",
+  $(@bind rerun_large LargeCheckBox(text="⟵ Click to re-run the <code>LargeSystem</code> evaluation.", default=parse(Bool, get(ENV, "AA228V_CI_RUN_EVALS", "false"))))""",
 	Markdown.parse("""
 	↑ This will re-run **`estimate_reachable_sets(::LargeSystem, ψ)`** and re-save **`$(get_filename(sys_large, ThisProject))`**
 
